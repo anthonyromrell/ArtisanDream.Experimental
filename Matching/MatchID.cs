a66e707dd2a1f42ea4e2f4b55b403551fbe54f09
Anthony Romrell <anthonyromrell@mac.com>
2018-10-05 17:53:09 -0600

    Added Words and Matching

---
+++ Matching/MatchID.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MatchID : MonoBehaviour
+{
+    public NameID ID;
+    public UnityEvent OnMatch;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var id = other.GetComponent<ObjectID>().ID;
+        Invoke(id);
+    }
+
+    public void Invoke(NameID id)
+    {
+        if (id == ID)
+        {
+            OnMatch.Invoke();
+        }
+    }
+}

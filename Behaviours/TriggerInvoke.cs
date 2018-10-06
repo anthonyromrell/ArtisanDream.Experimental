511c3bdd925ec812a886e3710ad23253bbe18d27
Anthony Romrell <anthonyromrell@mac.com>
2018-10-06 13:14:40 -0600

    Update Sub

--- Behaviours/TriggerInvoke.cs
+++ Behaviours/TriggerInvoke.cs
@@ -1,14 +1,1 @@
-﻿using ArtisanDream.Experimental.Color;
-using UnityEngine;
-
-namespace ArtisanDream.Experimental.Behaviours
-{
-	public InvokeBase InvokeObject;
-	
-	private void OnTriggerEnter(Collider other)
-	{
-		InvokeObject.Invoke(other.GetComponent<SpriteRenderer>());
-		print(other);
-		//ChangeColor.Invoke(other.GetComponent<SpriteRenderer>());
-	}
-}
+﻿

0608e39d3379ee5c537fe60c793643d3511f5ded
Anthony Romrell <anthonyromrell@mac.com>
2018-11-17 14:41:54 -0700

    Added Restart to Counter

--- Counter/PerpetualCounter.cs
+++ Counter/PerpetualCounter.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class PerpetualCounter : MonoBehaviour
- {
- 	public UnityEvent OnCount;
- 	public FloatData Seconds;
- 
- 	IEnumerator Start () {
- 		while (true)
- 		{
- 			OnCount.Invoke();
- 			yield return new WaitForSeconds(Seconds.Value);
- 		}
- 	}
- }
+{
+    public UnityEvent OnCount;
+    public FloatData Seconds;
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            OnCount.Invoke();
+            yield return new WaitForSeconds(Seconds.Value);
+        }
+    }
+
+    public void Restart()
+    {
+        StartCoroutine(Start());
+    }
+}

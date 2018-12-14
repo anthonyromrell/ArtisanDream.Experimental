5c671341a32fb5c5cd91a8f5e0f172c8ea01f5ea
Anthony Romrell <anthonyromrell@mac.com>
2018-12-13 20:48:27 -0700

    LevelLoader

---
+++ Utilties/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "Utilities/Game LevelLoader")]
+public class LevelLoader : ScriptableObject
+{
+    public void Load(Object scene)
+    {
+        var newScene = scene.name;
+        SceneManager.LoadScene(newScene);
+    }
+}
